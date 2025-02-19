4935e211dcc6976a401d58fe0cdfccb569156395
Kevin <josungtse@hotmail.co.uk>
2025-02-19 22:12:12 +0000

    removed my mongoDB links

--- Backend/test/contacts.test.js
+++ Backend/test/contacts.test.js
@@ -9,8 +9,8 @@ describe('Contacts API', () => {
   });
 
   it('should GET a contact by ID', async () => {
-    // Replace with an actual ID from DB before running tests
-    const res = await request(app).get('/contacts/67b62f6f86fb9394ca1ea47f'); 
+    // Replace the `id` beloew with an actual ID from DB before running tests
+    const res = await request(app).get('/contacts/id'); 
     expect(res.status).toBe(200);
     expect(res.body).toHaveProperty('firstName');
   });
@@ -32,18 +32,18 @@ describe('Contacts API', () => {
 
   it('should PATCH a contact', async () => {
     const res = await request(app)
-   // Replace with an actual ID from DB before running tests
-      .patch('/contacts/67b62f6f86fb9394ca1ea47f') 
+   // Replace the `id` beloew with an actual ID from DB before running tests
+      .patch('/contacts/id') 
       .send({ firstName: 'Updated' });
     expect(res.status).toBe(200);
     expect(res.body).toHaveProperty('firstName', 'Updated');
   });
 
   it('should DELETE a contact', async () => {
-    // Replace with an actual ID from DB before running tests
-    const res = await request(app).delete('/contacts/67b62f6f86fb9394ca1ea47f'); 
+    // Replace the `id` beloew with an actual ID from DB before running tests
+    const res = await request(app).delete('/contacts/id'); 
     expect(res.status).toBe(200);
-    // Replace with an actual ID from DB before running tests
-    expect(res.body.message).toBe('Delete Contact with ID 67b62f6f86fb9394ca1ea47f');
+    /// Replace the `id` beloew with an actual ID from DB before running tests
+    expect(res.body.message).toBe('Delete Contact with ID: id');
   });
 });
